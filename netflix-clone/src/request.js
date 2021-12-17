e6ef72a4a5cd71ad5e0749d4b06f7d9ec5fdd2b2
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2021-12-17 14:15:14 -0500

    added axios to make requests to movie database

---
+++ netflix-clone/src/request.js
@@ -0,0 +1,16 @@
+const APIKEY= "b307bce2eeedb77685311913f9acbc43";
+
+// api requests
+const requests = {
+fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&width_networks=213`,
+fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en_US`,
+fetchActionMovies: `/discover/movie?api_key=${API_KEY}&width_genres=28`,
+fetchComedyMovies: `/discover/tv?api_key=${API_KEY}&width_genres35`,
+fetchNHorrorMovies: `/discover/tv?api_key=${API_KEY}&width_genres=27`,
+fetchRomanceMovies: `/discover/tv?api_key=${API_KEY}&width_genres=10749`,
+fetchDocumentaries: `/discover/tv?api_key=${API_KEY}&width_genres=99`,
+
+}
+
+export default requests;
