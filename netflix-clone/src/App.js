ddb70bea159e50a458dcab0956dd82b32c9002bf
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2021-12-28 13:14:41 -0500

    added a banner compoent which will evntually display the banner '1.17'

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
 import "./App.css";
 import Row from "./Row";
-import request from "./request";
+import requests from "./requests";
+import Banner from "./Banner";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is an amzing test</h1>
-      <Row title="NETFLIX ORIGINALS" 
-      fetchUrl={request.fetchNetflixOriginals} 
-      isLargeRow
+      {/* Nav Bar*/}
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
-  
-      <Row title="TRENDING NOW" fetchUrl={request.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
+
+      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
