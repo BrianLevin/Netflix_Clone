9cc111f9e399604d0dc72759380a2d22953837c0
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2021-12-30 15:31:19 -0500

    changed request tag

--- netflix-clone/src/requests.js
+++ netflix-clone/src/requests.js
@@ -4,7 +4,7 @@ const API_KEY= "b307bce2eeedb77685311913f9acbc43";
 const requests = {
 fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
 fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&width_networks=213`,
-fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en_US`,
+fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
 fetchActionMovies: `/discover/movie?api_key=${API_KEY}&width_genres=28`,
 fetchComedyMovies: `/discover/tv?api_key=${API_KEY}&width_genres35`,
 fetchHorrorMovies: `/discover/tv?api_key=${API_KEY}&width_genres=27`,
